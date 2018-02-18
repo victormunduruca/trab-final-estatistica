b849cb73a584311b77868248f837674b2ddcb1c1
Victor Munduruca <victormunduruca@gmail.com>
2018-02-18 20:49:40 -0300

    Erro

--- trabalho3-prob.R
+++ trabalho3-prob.R
@@ -4,11 +4,17 @@ library("readxl")
 dados <- read_excel("dados_trabalho.xls")
 
 
-
+dados$grau_dependencia
 attach(dados)
 str(dados)
 
-grau_dependencia
+
+
 
 tab_grau = table(grau_dependencia)
-median(tab_grau)
+mean(grau_dependencia) # da erro ???
+
+amostra = sample(grau_dependencia, 2, replace = FALSE, prob = NULL)
+mean(amostra)
+
+
