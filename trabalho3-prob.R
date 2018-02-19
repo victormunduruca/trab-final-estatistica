7338f5ebac385c625eac6e19ac6166cc00c2444d
Victor Munduruca <victormunduruca@gmail.com>
2018-02-19 00:46:31 -0300

    Primeira questão (Bug do 100)

--- trabalho3-prob.R
+++ trabalho3-prob.R
@@ -1,4 +1,4 @@
-install.packages("readxl")
+#install.packages("readxl")
 library("readxl")
 
 dados <- read_excel("dados_trabalho.xls")
@@ -10,11 +10,23 @@ str(dados)
 
 
 
-
 tab_grau = as.numeric(grau_dependencia)
-mean(tab_grau) # da erro ???
+mean(tab_grau) 
+contadores = c(0,0,0,0,0,0)
+i = 1
+for(tamanho_amostra in c(20, 30, 50, 80, 120, 150)) {
+  for(j in 1:100) {
+    amostra = as.numeric(sample(grau_dependencia, tamanho_amostra, replace = FALSE, prob = NULL))
+    media_amostra = mean(amostra)
+    esquerda = t.test(amostra)$conf[1]
+    direita = t.test(amostra)$conf[2]
+    if(media_amostra > esquerda && media_amostra < direita) {
+      contadores[i] = contadores[i]+1
+    }
+  }
+  i = i + 1
+}
 
-amostra = sample(grau_dependencia, 2, replace = FALSE, prob = NULL)
-mean(amostra)
+
 
 
